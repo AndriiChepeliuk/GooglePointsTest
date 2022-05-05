3cf3ce24d2722c5c9308cde9a49b06f9a91fc8e2
Andrii <andrewchepeliuk@gmail.com>
2022-05-05 20:21:56 +0300

    Created PointCreationDTO

---
+++ Core/DTO/PointDTO/PointCreationDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Core.DTO.PointDTO
+{
+    public class PointCreationDTO
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string Address { get; set; }
+        public string Settlement { get; set; }
+        public string Region { get; set; }
+    }
+}
--- Infrastructure/Data/ApplicationContext.cs
+++ Infrastructure/Data/ApplicationContext.cs
@@ -1,11 +1,6 @@
 ﻿using Core.Entities.PointEntity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Infrastructure.Data
 {
