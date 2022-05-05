778ca310119a56a51f1362cac9c2f54bec4c64f8
Andrii <andrewchepeliuk@gmail.com>
2022-05-05 20:17:51 +0300

    Repository

---
+++ Infrastructure/Repository/Repository.cs
@@ -0,0 +1,64 @@
+﻿using Core.Interfaces;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repository
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        private ApplicationContext _context;
+        private DbSet<TEntity> _dbSet;
+
+        public Repository(ApplicationContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<TEntity>();
+        }
+
+        public async Task DeleteAsync(TEntity entityToDelete)
+        {
+            await Task.Run(() =>
+            {
+                if (_context.Entry(entityToDelete).State == EntityState.Detached)
+                {
+                    _dbSet.Attach(entityToDelete);
+                }
+                _dbSet.Remove(entityToDelete);
+            });
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<TEntity> GetByIdAsync<TKey>(TKey id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public async Task<TEntity> InsertAsync(TEntity entity)
+        {
+            return (await _dbSet.AddAsync(entity)).Entity;
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(TEntity entityToUpdate)
+        {
+            await Task.Run(() =>
+            {
+                _dbSet.Attach(entityToUpdate);
+                _context.Entry(entityToUpdate).State = EntityState.Modified;
+            });
+        }
+    }
+}
