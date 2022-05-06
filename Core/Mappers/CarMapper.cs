ef1fdc36a6e1c44534f07592abbaef2372c3b19f
Andrii <andrewchepeliuk@gmail.com>
2022-05-06 17:21:24 +0300

    Created CarMapper and Point Service

---
+++ Core/Mappers/CarMapper.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Core.DTO.PointDTO;
+using Core.Entities.PointEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Mappers
+{
+    public class CarMapper : Profile
+    {
+        public CarMapper()
+        {
+            CreateMap<PointCreationDTO, Point>();
+        }
+    }
+}
---
+++ Core/Services/PointService.cs
@@ -0,0 +1,27 @@
+﻿using Core.DTO.PointDTO;
+using Core.Entities.PointEntity;
+using Core.Interfaces;
+using Core.Interfaces.CustomService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Services
+{
+    public class PointService : IPointService
+    {
+        private readonly IRepository<Point> _pointRepository;
+
+        public PointService(IRepository<Point> repository)
+        {
+            _pointRepository = repository;
+        }
+
+        public async Task<PointCreationDTO> AddPoint(PointCreationDTO pointCreationDTO)
+        {
+            return await _pointRepository.InsertAsync(pointCreationDTO);
+        }
+    }
+}
